cf6eb255999a229dd062de0f5dff7ded3c3ad395
Aftab Hassan <afhassan@microsoft.com>
2018-03-12 19:52:28 -0700

    kafka producer

---
+++ KafkaProducerRDKAFKA/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RdKafka;
+
+namespace KafkaProducerRDKAFKA
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string brokerList = "localhost";//args[0];
+            string topicName = "topicName";//args[1];
+
+            using (Producer producer = new Producer(brokerList))
+            using (Topic topic = producer.Topic(topicName))
+            {
+                Console.WriteLine($"{producer.Name} producing on {topic.Name}. q to exit.");
+
+                string text;
+                while ((text = Console.ReadLine()) != "q")
+                {
+                    byte[] data = Encoding.UTF8.GetBytes(text);
+                    Task<DeliveryReport> deliveryReport = topic.Produce(data);
+                    var unused = deliveryReport.ContinueWith(task =>
+                    {
+                        Console.WriteLine($"Partition: {task.Result.Partition}, Offset: {task.Result.Offset}");
+                    });
+                }
+            }
+        }
+    }
+}
